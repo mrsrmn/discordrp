ac931db12ac9223fab24dad6d3738df546da6a16
Emir Sürmen <63517174+MakufonSkifto@users.noreply.github.com>
2021-03-29 16:21:56 +0300

    Update Form1.Designer.cs

--- discordrp/Form1.Designer.cs
+++ discordrp/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace discordrp
+namespace discordrp
 {
     partial class Form1
     {
@@ -29,18 +29,346 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button = new System.Windows.Forms.Button();
+            this.clientId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.detailsBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.stateBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timestampBox = new System.Windows.Forms.CheckBox();
+            this.smallImageKey = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.largeImageText = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.largeImageKey = new System.Windows.Forms.TextBox();
+            this.smallImageText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.Color.GreenYellow;
+            this.button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.button.Location = new System.Drawing.Point(11, 418);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(140, 38);
+            this.button.TabIndex = 0;
+            this.button.Text = "Start RP";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // clientId
+            // 
+            this.clientId.Location = new System.Drawing.Point(149, 12);
+            this.clientId.Name = "clientId";
+            this.clientId.Size = new System.Drawing.Size(207, 20);
+            this.clientId.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.label2.ForeColor = System.Drawing.Color.Silver;
+            this.label2.Location = new System.Drawing.Point(11, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Client ID";
+            // 
+            // detailsBox
+            // 
+            this.detailsBox.Location = new System.Drawing.Point(150, 41);
+            this.detailsBox.Name = "detailsBox";
+            this.detailsBox.Size = new System.Drawing.Size(206, 20);
+            this.detailsBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.label3.ForeColor = System.Drawing.Color.Silver;
+            this.label3.Location = new System.Drawing.Point(11, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Details";
+            // 
+            // stateBox
+            // 
+            this.stateBox.Location = new System.Drawing.Point(150, 76);
+            this.stateBox.Name = "stateBox";
+            this.stateBox.Size = new System.Drawing.Size(207, 20);
+            this.stateBox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.label4.ForeColor = System.Drawing.Color.Silver;
+            this.label4.Location = new System.Drawing.Point(11, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 19);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "State";
+            // 
+            // timestampBox
+            // 
+            this.timestampBox.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.timestampBox.ForeColor = System.Drawing.Color.Silver;
+            this.timestampBox.Location = new System.Drawing.Point(14, 118);
+            this.timestampBox.Name = "timestampBox";
+            this.timestampBox.Size = new System.Drawing.Size(115, 25);
+            this.timestampBox.TabIndex = 8;
+            this.timestampBox.Text = "Enable Timestamp";
+            this.timestampBox.UseVisualStyleBackColor = true;
+            // 
+            // smallImageKey
+            // 
+            this.smallImageKey.Location = new System.Drawing.Point(149, 161);
+            this.smallImageKey.Name = "smallImageKey";
+            this.smallImageKey.Size = new System.Drawing.Size(207, 20);
+            this.smallImageKey.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.label7.ForeColor = System.Drawing.Color.Silver;
+            this.label7.Location = new System.Drawing.Point(12, 161);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(139, 19);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Small Image Key";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (162)));
+            this.label8.ForeColor = System.Drawing.Color.Silver;
+            this.label8.Location = new System.Drawing.Point(12, 194);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(139, 19);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Small Image Text";
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.label9.ForeColor = System.Drawing.Color.Silver;
+            this.label9.Location = new System.Drawing.Point(11, 270);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(139, 19);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Large Image Text";
+            // 
+            // largeImageText
+            // 
+            this.largeImageText.Location = new System.Drawing.Point(149, 270);
+            this.largeImageText.Name = "largeImageText";
+            this.largeImageText.Size = new System.Drawing.Size(207, 20);
+            this.largeImageText.TabIndex = 17;
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.label10.ForeColor = System.Drawing.Color.Silver;
+            this.label10.Location = new System.Drawing.Point(12, 232);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(139, 19);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Large Image Key";
+            // 
+            // largeImageKey
+            // 
+            this.largeImageKey.Location = new System.Drawing.Point(149, 232);
+            this.largeImageKey.Name = "largeImageKey";
+            this.largeImageKey.Size = new System.Drawing.Size(207, 20);
+            this.largeImageKey.TabIndex = 15;
+            // 
+            // smallImageText
+            // 
+            this.smallImageText.Location = new System.Drawing.Point(150, 194);
+            this.smallImageText.Name = "smallImageText";
+            this.smallImageText.Size = new System.Drawing.Size(206, 20);
+            this.smallImageText.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(89, 371);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 19);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "STATUS";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 320);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(72, 70);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Turquoise;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.button1.Location = new System.Drawing.Point(157, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 38);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Update RP";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.label5.ForeColor = System.Drawing.Color.Silver;
+            this.label5.Location = new System.Drawing.Point(89, 320);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 19);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "USERNAME";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label6.ForeColor = System.Drawing.Color.Silver;
+            this.label6.Location = new System.Drawing.Point(139, 371);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 25);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Offline";
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.labelUsername.ForeColor = System.Drawing.Color.Silver;
+            this.labelUsername.Location = new System.Drawing.Point(89, 339);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(457, 25);
+            this.labelUsername.TabIndex = 25;
+            this.labelUsername.Text = "--------------------------------";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.buttonSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSave.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.buttonSave.Location = new System.Drawing.Point(303, 418);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(140, 38);
+            this.buttonSave.TabIndex = 26;
+            this.buttonSave.Text = "Save Settings";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.BackColor = System.Drawing.Color.Gold;
+            this.buttonReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonReset.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.buttonReset.Location = new System.Drawing.Point(449, 418);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(107, 38);
+            this.buttonReset.TabIndex = 27;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = false;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (64)))), ((int) (((byte) (64)))), ((int) (((byte) (64)))));
+            this.ClientSize = new System.Drawing.Size(568, 473);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.smallImageText);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.largeImageText);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.largeImageKey);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.smallImageKey);
+            this.Controls.Add(this.timestampBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.stateBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.detailsBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.clientId);
+            this.Controls.Add(this.button);
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(584, 512);
+            this.MinimumSize = new System.Drawing.Size(584, 512);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Discord);
+            this.Text = "Discord Rich Presence";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button buttonReset;
+
+        private System.Windows.Forms.Button buttonSave;
+
+        private System.Windows.Forms.Label labelUsername;
+
+        private System.Windows.Forms.Label label6;
+
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.TextBox smallImageText;
+
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox largeImageText;
+        private System.Windows.Forms.TextBox largeImageKey;
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox smallImageKey;
+
+        private System.Windows.Forms.CheckBox timestampBox;
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox stateBox;
+
+        private System.Windows.Forms.TextBox detailsBox;
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox clientId;
+
+        private System.Windows.Forms.Button button;
 
         #endregion
     }
